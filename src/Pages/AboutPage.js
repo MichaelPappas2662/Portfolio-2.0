88d4dc9a735f65b553e0efa23acd0d87446d4dc9
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-22 16:02:45 +1000

    starting on aboutMe css

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Title from '../Components/Title'
+import ImageSection from '../Components/ImageSection'
 
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Title title={'About Me'} span={'About Me'}/>   
+            <ImageSection />
         </div>
     )
 }
