62997b2b33db7783acf68f85c4208a31fd221d24
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-22 16:06:57 +1000

    added more code on app and css

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 import './App.scss';
 import NavBar from './Components/NavBar';
 import HomePage from './Pages/HomePage';
+import {Switch, Route} from 'react-router-dom';
 import AboutPage from './Pages/AboutPage';
 import PortfoliosPage from './Pages/PortfoliosPage';
 import BlogsPage from './Pages/BlogsPage';
-import ContactPage from './Pages/Contact';
-import {Switch, Route} from 'react-router-dom';
+import ContactPage from './Pages/ContactPage';
+import { useState } from 'react';
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () =>{
+    setNavToggle(!navToggle)
+  }
+
   return (
     <div className="App">
-      <div className="sidebar">
+      <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
         <NavBar />
       </div>
+      <div className="nav-btn" onClick={navClick}>
+        <div className="lines-1"></div>
+        <div className="lines-2"></div>
+        <div className="lines-3"></div>
+      </div>
       <div className="main-content">
-        <div className='content'>
-          <Switch>
-            <Route path='/' exact>
-              <HomePage />
-            </Route>
-            <Route path='/about' exact>
-              <AboutPage />
-            </Route>
-            <Route path='/portfolios' exact>
-              <PortfoliosPage />
-            </Route>
-            <Route path='/blogs' exact>
-              <BlogsPage />
-            </Route>
-            <Route path='/contact' exact>
-              <ContactPage />
-            </Route>
-          </Switch>
-        </div>
+          <div className="content">
+            <Switch>
+              <Route path="/" exact>
+                <HomePage />
+              </Route>
+              <Route path="/about" exact>
+                <AboutPage />
+              </Route>
+              <Route path="/portfolios" exact>
+                <PortfoliosPage />
+              </Route>
+              <Route path="/blogs" exact>
+                <BlogsPage />
+              </Route>
+              <Route path="/contact" exact>
+                <ContactPage />
+              </Route>
+            </Switch>
+          </div>
       </div>
     </div>
   );
