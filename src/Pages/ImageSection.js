9caaeb59d8168f9b6d2c1c92428a656c8f016ff3
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-22 16:02:26 +1000

    finishing imageSection

--- src/Pages/ImageSection.js
+++ src/Pages/ImageSection.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import about from '../img/about.png'
+import React from 'react';
+import about from '../img/about.jpg';
 
 function ImageSection() {
     return (
         <div className="ImageSection">
             <div className="img">
-                <img src={about} alt="" />
+                <img src={about} alt=""/>
             </div>
             <div className="about-info">
-                <h4>I am <span>Lorem ipsum</span></h4>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                    Tempora provident sequi qui, quidem ducimus nobis?
+                <h4>I am<span> Lorem Ipsum</span></h4>
+                <p className="about-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Voluptatum necessitatibus perferendis numquam dolorem cumque facilis.
+                    Desir per po la ta doso vi slone ve?
                 </p>
                 <div className="about-details">
-                    <p>
-                       <div className="about-details">
-                            <p>
-                            </p>   
-                        </div> 
-                    </p>
+                    <div className="left-section">
+                        <p>Full Name</p>
+                        <p>Age</p>
+                        <p>Nationality</p>
+                        <p>Languages</p>
+                        <p>Address</p>
+                        <p>Country</p>
+                    </div>
+                    <div className="right-section">
+                        <p>: Michael Pappas</p>
+                        <p>: 32</p>
+                        <p>: Greek</p>
+                        <p>: Greek, Spanish, English</p>
+                        <p>: Parramatta, NSW</p>
+                        <p>: Australia</p>
+                    </div>
                 </div>
+                <button className="btn">Download Cv</button>
             </div>
         </div>
     )
 }
 
-export default ImageSection
+export default ImageSection;
