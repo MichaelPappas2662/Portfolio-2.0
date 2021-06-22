12446f7d4c5f0bb7dd8fb923520bf3dddbdbd978
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-22 17:05:39 +1000

    adding the styles inside the imagesection.js

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,41 +1,90 @@
-import React from 'react';
-import about from '../img/about.jpg';
+import React from 'react'
+import styled from 'styled-components';
+import resume from '../img/resume.jpg';
+import PrimaryButton from './PrimaryButton';
 
 function ImageSection() {
     return (
-        <div className="ImageSection">
-            <div className="img">
-                <img src={about} alt=""/>
+        <ImageSectionStyled>
+            <div className="left-content">
+                <img src={resume} alt=""/>
             </div>
-            <div className="about-info">
-                <h4>I am<span> Lorem Ipsum</span></h4>
-                <p className="about-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Voluptatum necessitatibus perferendis numquam dolorem cumque facilis.
-                    Desir per po la ta doso vi slone ve?
+            <div className="right-content">
+                <h4>I am <span>Lorem Ipsum</span></h4>
+                <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, natus quas vero enim 
+                    praesentium delectus est id fugiat ab libero adipisci recusandae at maxime veritatis! 
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, nesciunt.
+                    
                 </p>
-                <div className="about-details">
-                    <div className="left-section">
+                <div className="about-info">
+                    <div className="info-title">
                         <p>Full Name</p>
                         <p>Age</p>
-                        <p>Nationality</p>
-                        <p>Languages</p>
-                        <p>Address</p>
-                        <p>Country</p>
+                        <p>Nationality </p>
+                        <p>Languages </p>
+                        <p>Location</p>
+                        <p>Service</p>
                     </div>
-                    <div className="right-section">
+                    <div className="info">
                         <p>: Michael Pappas</p>
                         <p>: 32</p>
-                        <p>: Greek</p>
-                        <p>: Greek, Spanish, English</p>
-                        <p>: Parramatta, NSW</p>
-                        <p>: Australia</p>
+                        <p>: Greek </p>
+                        <p>: Greek, French, English </p>
+                        <p>: Sydney, Australia</p>
+                        <p>: Freelance</p>
                     </div>
                 </div>
-                <button className="btn">Download Cv</button>
+                <PrimaryButton title={'Download Cv'} />
             </div>
-        </div>
+        </ImageSectionStyled>
     )
 }
 
+
+const ImageSectionStyled = styled.div`
+    margin-top: 5rem;
+    display: flex;
+    @media screen and (max-width:1000px){
+        flex-direction: column;
+        .left-content{
+            margin-bottom: 2rem;
+        }
+    }
+    .left-content{
+        width: 100%;
+        img{
+            width: 95%;
+            object-fit: cover;
+        }
+    }
+    .right-content{
+        width: 100%;
+        h4{
+            font-size: 2rem;
+            color: var(--white-color);
+            span{
+                font-size: 2rem;
+            }
+        }
+        .paragraph{
+            padding: 1rem 0;
+        }
+        .about-info{
+            display: flex;
+            padding-bottom: 1.4rem;
+            .info-title{
+                padding-right: 3rem;
+                p{
+                    font-weight: 600;
+                }
+            }
+            .info-title, .info{
+                p{
+                    padding: .3rem 0;
+                }
+            }
+        }
+    }
+`;
 export default ImageSection;
