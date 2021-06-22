3e78ce71a3bec1c78cc8b2201799ba2e0b372e71
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-22 16:14:30 +1000

    added more code on index main page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
-    <App/>
+      <App />
     </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
-
