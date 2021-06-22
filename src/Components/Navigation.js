4c00511bfbfc702d6222be93716a060120de692b
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-22 16:43:30 +1000

    reforging the code in NavBar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,51 +1,113 @@
 import React from 'react'
-import avatar from '../img/avatar.png';
+import styled from 'styled-components';
 import {NavLink} from 'react-router-dom';
+import avatar from '../img/avatar.jpg';
 
-function NavBar() {
+function Navigation() {
     return (
-        <div className='NavBar'>
-            <nav className='nav'>
-            <div className='profile'>
+        <NavigationStyled>
+            <div className="avatar">
                 <img src={avatar} alt=""/>
             </div>
-                <ul className='nav-items'>
-                    <li className='nav-item'>
-                        <NavLink to="/" exact activeClassName="active">
-                            HOME
-                        </NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink to="/about" exact activeClassName="active">
-                           About
-                        </NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink to="/portfolios" exact activeClassName="active">
-                            Portfolios
-                        </NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink to="/blogs" exact activeClassName="active">
-                           Blogs
-                        </NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink to="/contact" exact activeClassName="active">
-                            Contact
-                        </NavLink>
-                    </li>
-                </ul>
-                
-                <footer className='footer'>
-                    <p>
-                        @2021 lorem ipsum dolor sit 
-                    </p>
-                </footer>
-            </nav>
-            
-        </div>
+            <ul className="nav-items">
+                <li className="nav-item">
+                    <NavLink to="/" activeClassName="active-class" exact>Home</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/about" activeClassName="active-class" exact>About</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/resume" activeClassName="active-class" exact>Resume</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/portfolios" activeClassName="active-class" exact>Portfolios</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/blogs" activeClassName="active-class" exact>Blogs</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/contact" activeClassName="active-class" exact>Contact</NavLink>
+                </li>
+            </ul>
+            <footer className="footer">
+                <p>@2021 <b>Lorem Ipsum</b></p>
+            </footer>
+        </NavigationStyled>
     )
 }
 
-export default NavBar
+const NavigationStyled = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    border-right: 1px solid var(--border-color);
+    .avatar{
+        width: 100%;
+        border-bottom: 1px solid var(--border-color);
+        text-align: center;
+        padding: 1rem 0;
+        img{
+            width: 70%;
+            border-radius: 50%;
+            border: 8px solid var(--border-color);
+        }
+    }
+
+    .nav-items{
+        width: 100%;
+        text-align: center;
+        .active-class{
+            background-color: var(--primary-color-light);
+            color: white;
+        }
+        li{
+            display: block;
+            a{
+                display: block;
+                padding: .45rem 0;
+                position: relative;
+                z-index: 10;
+                text-transform: uppercase;
+                transition: all .4s ease-in-out;
+                font-weight: 600;
+                letter-spacing: 1px;
+                &:hover{
+                    cursor: pointer;
+                    color: var(--white-color);
+                }
+                &::before{
+                    content: "";
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                    width: 0;
+                    height: 50%;
+                    background-color: var( --primary-color);
+                    transition: All 0.4s cubic-bezier(1,-0.2,.25,.95) ;
+                    opacity: 0.21;
+                    z-index: -1;
+                }
+            }
+
+            a:hover::before{
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+
+    footer{
+        border-top: 1px solid var(--border-color);
+        width: 100%;
+        p{
+            padding: 1.3rem 0;
+            font-size: 1.1rem;
+            display: block;
+            text-align: center;
+        }
+    }
+`;
+export default Navigation;
