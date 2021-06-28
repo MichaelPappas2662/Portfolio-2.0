45cfbfdd40f7c70db6f7f1079ecafd2daedb1120
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-28 13:40:13 +1000

    adding more code on Global style

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -75,7 +75,104 @@ body{
     color: var(--font-light-color);
     transition: all .4s ease-in-out;
 }
-`;
 
+body::-webkit-scrollbar{
+    width: 9px;
+    background-color: #383838;
+}
+body::-webkit-scrollbar-thumb{
+    border-radius: 10px;
+    background-color: #6b6b6b;
+}
+body::-webkit-scrollbar-track{
+    border-radius: 10px;
+    background-color: #383838;
+}
+
+
+a{
+    font-family: inherit;
+    color: inherit;
+    font-size: inherit;
+    font-size: 1rem;
+}
+
+h1{
+    font-size: 4rem;
+    color: var(--white-color);
+    span{
+        font-size: 4rem;
+        @media screen and (max-width: 502px){
+            font-size: 3rem
+        }
+    }
+    @media screen and (max-width: 502px){
+        font-size: 3rem
+    }
+}
+
+span{
+    color: var(--primary-color);
+}
+h6{
+    color: var(--white-color);
+    font-size: 1.2rem;
+    padding-bottom: .6rem;
+}
+
+//Utilities
+.u-margin-bottom{
+    margin-bottom: 4rem;
+}
+
+
+//Floting Toggler
+.light-dark-mode{
+    position: fixed;
+    right: 0;
+    top: 50%;
+    background-color: var(--background-light-color-2);
+    width: 6.5rem;
+    height: 2.5rem;
+    z-index: 15;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg{
+      display: flex;
+      align-items: center;
+      font-size: 1.7rem;
+      color: var(--white-color);
+    }
+  }
+
+  //Nav Toggler
+  .ham-burger-menu{
+      position: absolute;
+      right: 5%;
+      top: 3%; 
+      display: none;
+      z-index: 15;
+      svg{
+          font-size: 3rem;
+      }
+    }
+
+    .nav-toggle{
+        transform: translateX(0);
+        z-index: 20;
+    }
+  @media screen and (max-width:1200px){
+    .ham-burger-menu{
+        display: block;
+    }
+  }
+
+
+//Global Media Queries
+
+
+
+`;
 
 export default GlobalStyle;
