b21eef24d6076093d230616f2b2f8cf3fd36b6b6
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-28 12:37:50 +1000

    finished the style in contact page

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -55,6 +55,71 @@ function ContactPage() {
     )
 }
 
-const ContactPageStyled = styled.section``;
+const ContactPageStyled = styled.section`
+    .contact-section{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-column-gap: 2rem;
+        @media screen and (max-width: 978px){
+            grid-template-columns: repeat(1, 1fr);
+            .f-button{
+                margin-bottom: 3rem;
+            }
+        }
+        .right-content{
+            display: grid;
+            grid-template-columns: repeat(1, 1fr);
+            @media screen and (max-width: 502px){
+                width: 70%;
+            }
+        }
+        .contact-title{
+            h4{
+                color: var(--white-color);
+                padding: 1rem 0;
+                font-size: 1.8rem;
+            }
+        }
+        .form{
+            width: 100%;
+            @media screen and (max-width: 502px){
+                width: 100%;
+            }
+            .form-field{
+                margin-top: 2rem;
+                position: relative;
+                width: 100%;
+                label{
+                    position: absolute;
+                    left: 20px;
+                    top: -19px;
+                    display: inline-block;
+                    background-color: var(--background-dark-color);
+                    padding:0 .5rem;
+                    color: inherit;
+                }
+                input{
+                    border: 1px solid var(--border-color);
+                    outline: none;
+                    background: transparent;
+                    height: 50px;
+                    padding:0 15px;
+                    width: 100%;
+                    color: inherit;
+                }
+                textarea{
+                    background-color: transparent;
+                    border: 1px solid var(--border-color);
+                    outline: none;
+                    color: inherit;
+                    width: 100%;
+                    padding: .8rem 1rem;
+                }
+            }
+
+            
+        }
+    }
+`;
 
 export default ContactPage;
